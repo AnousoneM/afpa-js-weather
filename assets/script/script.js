308d6207405ea7edb04f585fee98f3f70a3db569
Anousone M. <anousone.mounivongs@gmail.com>
2025-01-17 01:00:14 +0100

    mise à jour

--- assets/script/script.js
+++ assets/script/script.js
@@ -16,37 +16,20 @@ const apiKey = '5593f162ed8c4c3607df1a94e144abb5'
 // url permettant d'obtenir les prévisions météo
 let url = `https://api.openweathermap.org/data/2.5/forecast?lang=fr&units=metric&q=${city}&appid=${apiKey}`
 
+
+
+
+
 // on va chercher les infos de l'url
 fetch(url)
     .then(response => response.json())
     .then(data => {
 
-        // affiche le json en entier
-        console.log(data)
-        // affiche le nom de la ville
-        console.log(data.city.name)
-        // date du jour avec moment.js
-        console.log(moment(data.list[0].dt_txt).locale('fr').format('LL'))
-        // heure avec moment .js
-        console.log(moment(data.list[0].dt_txt).locale('fr').format('LT'))
-        // icone du temps
-        console.log(data.list[0].weather[0].icon)
-        // description du temps
-        console.log(data.list[0].weather[0].description)
-        // température
-        console.log(data.list[0].main.temp.toPrecision(2))
-        // température ressentie
-        console.log(data.list[0].main.feels_like.toPrecision(2))
-        // température min.
-        console.log(data.list[0].main.temp_min.toPrecision(2))
-        // température max.
-        console.log(data.list[0].main.temp_max.toPrecision(2))
-        // levé du soleil
-        let sunrise = new Date(data.city.sunrise * 1000)
-        console.log(moment(sunrise).locale('fr').format('LT'))
-        // couché du soleil
-        let sunset = new Date(data.city.sunset * 1000)
-        console.log(moment(sunset).locale('fr').format('LT'))
+
+
+        const fiveDays = data.list.filter( date => date.dt_txt.split(' ')[1] == '12:00:00')
+
+        console.log(caca)
 
         document.querySelector('#interface').innerHTML = `
         <p class="fs-1 mt-2 mb-1 fw-bold">${data.city.name}</p>
