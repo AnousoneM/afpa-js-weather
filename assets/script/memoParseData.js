f77fd81436e5a7b6af1c1a4d66819cb47be272e6
Anousone M. <anousone.mounivongs@gmail.com>
2025-01-18 00:52:44 +0100

    mise à jour avec prev sur 5 jours'

--- assets/script/memoParseData.js
+++ assets/script/memoParseData.js
@@ -1,30 +1,46 @@
         // permet de connaitre le code de recherche
         console.log(data.cod)
+
         // affiche le json en entier
         console.log(data)
+
         // affiche le nom de la ville
         console.log(data.city.name)
+
         // date du jour avec moment.js
         console.log(moment(data.list[0].dt_txt).locale('fr').format('LL'))
+
+        // jour avec moment.js
+        console.log(moment(data.list[0].dt_txt).locale('fr').format('dddd'))
+
         // heure avec moment .js
         console.log(moment(data.list[0].dt_txt).locale('fr').format('LT'))
+
         // icone du temps
         console.log(data.list[0].weather[0].icon)
+
         // description du temps
         console.log(data.list[0].weather[0].description)
+
         // température
         console.log(data.list[0].main.temp.toPrecision(2))
+
         // température ressentie
         console.log(data.list[0].main.feels_like.toPrecision(2))
+
         // température min.
         console.log(data.list[0].main.temp_min.toPrecision(2))
+
         // température max.
         console.log(data.list[0].main.temp_max.toPrecision(2))
+
         // levé du soleil
         let sunrise = new Date(data.city.sunrise * 1000)
         console.log(moment(sunrise).locale('fr').format('LT'))
+
         // couché du soleil
         let sunset = new Date(data.city.sunset * 1000)
         console.log(moment(sunset).locale('fr').format('LT'))
+
         // prévisions des 5 jours / 12:00
         console.log(data.list.filter( date => date.dt_txt.split(' ')[1] == '12:00:00'))
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,5 @@
 // import { apiKey } from "../../config.js"
 
-console.log('ok')
-
 // Afin de récupérer et manipuler les parametres d'URL nous allons créer un objet URLSearchParams
 const urlParams = new URLSearchParams(window.location.search);
 let city = urlParams.get('city');
@@ -11,11 +9,6 @@ if (city == null || city == "") {
     // dans le cas échéant nous attribuons une valeur par défaut : havre
     city = 'havre'
 }
-
-
-
-
-
 
 // clé API
 const apiKey = '5593f162ed8c4c3607df1a94e144abb5'
@@ -37,24 +30,61 @@ fetch(url)
             // sinon, cela indique que le ville a été trouvée, donc sinon on affiche
         } else {
             document.querySelector('#interface').innerHTML = `
-            <p class="fs-1 mt-2 mb-1 fw-bold">${data.city.name}</p>
-            <p class="fs-5">${moment(data.list[0].dt_txt).locale('fr').format('LL')}</p>
-    
-            <div class="py-2 mb-4 border bg-success shadow rounded mx-3">
-                <div class="row m-0">
-                    <div class="col">
-                        <img src="https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@4x.png" alt="Image météo">
-                    </div>
-                    <div class="col p-3 d-flex flex-column justify-content-center">
-                        <p class="fs-1 fw-bold text-light">${moment(data.list[0].dt_txt).locale('fr').format('LT')}</p>
-                        <p class="fs-1 mb-0 text-light">${data.list[0].weather[0].description}</p>
+
+        <p class="fs-1 mt-2 mb-1 fw-bold">${data.city.name}</p>
+        <p class="fs-5">${moment(data.list[0].dt_txt).locale('fr').format('LL')}</p>
+
+        <div class="mb-3 bg-success-subtle shadow rounded mx-3">
+            <div class="row m-0 p-2">
+
+                <div class="col">
+                    <img src="https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png" alt="Image météo">
+                    <p class="mb-0">${data.list[0].weather[0].description}</p>
+                </div>
+
+                <div class="col border-start border-secondary p-1 row m-0">
+                    <div class="col d-flex flex-column justify-content-center">
+                        <p class="mb-0"><span class="temperature">${data.list[0].main.temp.toPrecision(1)}°</span>C</p>
+                        <p class="feeling mb-0">Ressentie : ${data.list[0].main.feels_like.toPrecision(1)}°C</p>
                     </div>
                 </div>
+
             </div>
-    
-            <p class="fs-1 mb-0">${data.list[0].main.temp.toPrecision(2)}° C<i class="bi bi-thermometer ms-1"></i></p>
-            <p class="fst-italic">ressenti : ${data.list[0].main.feels_like.toPrecision(2)}° C</p>
-            <p><span class="text-primary">min :</span> ${data.list[0].main.temp_min.toPrecision(2)}° C / <span class="text-danger">max :</span> ${data.list[0].main.temp_max.toPrecision(2)}° C</p>
+        </div>
+
+        <div class="d-flex overflow-auto mx-3 p-2 mb-3 bg-success-subtle rounded shadow">
+
+            <div class="forecastHours bg-light border rounded py-2">
+                <p class="mb-0">12h00</p>
+                    <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="Image météo">
+                <p class="mb-0">12°C</p>
+            </div>
+            <div class="forecastHours bg-light border rounded py-2">
+                <p class="mb-0">12h00</p>
+                    <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="Image météo">
+                <p class="mb-0">12°C</p>
+            </div>
+
+        </div>
+
+        <div class="bg-success-subtle rounded shadow mx-3 p-2">        
+            
+            ${makeFiveDaysCast(data.list.filter( date => date.dt_txt.split(' ')[1] == '12:00:00'))}
+
+        </div>
+
         `
         }
     })
+
+    function makeFiveDaysCast(arrayData){
+        let htmlElement = ''
+        arrayData.forEach(element => {
+            htmlElement += `
+            <div class="forecastDays bg-light text-center d-flex justify-content-evenly align-items-center rounded mb-1">
+                ${moment(element.dt_txt).locale('fr').format('ddd')}<img src="https://openweathermap.org/img/wn/${element.weather[0].icon}.png" alt="Image météo"><span><i class="bi bi-arrow-down-short"></i>${element.main.temp_min.toPrecision(1)}°C / <i class="bi bi-arrow-up-short"></i>${element.main.temp_max.toPrecision(1)}°C</span>
+            </div>    
+            `
+        })
+        return htmlElement
+    }
