cb30946e338d24d3f5ad5984e8c469d1702ead33
Anousone M. <anousone.mounivongs@gmail.com>
2025-01-18 10:24:11 +0100

    mise à jour avec previsions heures et jours ok

--- assets/script/memoParseData.js
+++ assets/script/memoParseData.js
@@ -10,8 +10,8 @@
         // date du jour avec moment.js
         console.log(moment(data.list[0].dt_txt).locale('fr').format('LL'))
 
-        // jour avec moment.js
-        console.log(moment(data.list[0].dt_txt).locale('fr').format('dddd'))
+        // jour en mode short ex. ven. avec moment.js
+        console.log(moment(data.list[0].dt_txt).locale('fr').format('ddd'))
 
         // heure avec moment .js
         console.log(moment(data.list[0].dt_txt).locale('fr').format('LT'))
@@ -23,16 +23,16 @@
         console.log(data.list[0].weather[0].description)
 
         // température
-        console.log(data.list[0].main.temp.toPrecision(2))
+        console.log(Math.round(data.list[0].main.temp))
 
         // température ressentie
-        console.log(data.list[0].main.feels_like.toPrecision(2))
+        console.log(Math.round(data.list[0].main.feels_like))
 
         // température min.
-        console.log(data.list[0].main.temp_min.toPrecision(2))
+        console.log(Math.round(data.list[0].main.temp_min))
 
         // température max.
-        console.log(data.list[0].main.temp_max.toPrecision(2))
+        console.log(Math.round(data.list[0].main.temp_max))
 
         // levé du soleil
         let sunrise = new Date(data.city.sunrise * 1000)
@@ -42,5 +42,5 @@
         let sunset = new Date(data.city.sunset * 1000)
         console.log(moment(sunset).locale('fr').format('LT'))
 
-        // prévisions des 5 jours / 12:00
-        console.log(data.list.filter( date => date.dt_txt.split(' ')[1] == '12:00:00'))
+        // prévisions des 5 jours / 6:00 et 12:00
+        console.log(data.list.filter( date => date.dt_txt.split(' ')[1] == '12:00:00' || date.dt_txt.split(' ')[1] == '6:00:00'))
--- assets/script/script.js
+++ assets/script/script.js
@@ -44,8 +44,8 @@ fetch(url)
 
                 <div class="col border-start border-secondary p-1 row m-0">
                     <div class="col d-flex flex-column justify-content-center">
-                        <p class="mb-0"><span class="temperature">${data.list[0].main.temp.toPrecision(1)}°</span>C</p>
-                        <p class="feeling mb-0">Ressentie : ${data.list[0].main.feels_like.toPrecision(1)}°C</p>
+                        <p class="mb-0"><span class="temperature">${Math.round(data.list[0].main.temp)}°</span>C</p>
+                        <p class="feeling mb-0">Ressentie : ${Math.round(data.list[0].main.feels_like)}°C</p>
                     </div>
                 </div>
 
@@ -53,17 +53,8 @@ fetch(url)
         </div>
 
         <div class="d-flex overflow-auto mx-2 p-2 mb-3 bg-success-subtle rounded shadow">
-
-            <div class="forecastHours bg-light border rounded py-2">
-                <p class="mb-0">12h00</p>
-                    <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="Image météo">
-                <p class="mb-0">12°C</p>
-            </div>
-            <div class="forecastHours bg-light border rounded py-2">
-                <p class="mb-0">12h00</p>
-                    <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="Image météo">
-                <p class="mb-0">12°C</p>
-            </div>
+     
+            ${makeTenHoursCast(data, 10)}
 
         </div>
 
@@ -77,14 +68,33 @@ fetch(url)
         }
     })
 
+
+    // fonction permettant de créer les 5 prochains jours
+    // elle prends en paramètre un tableau de données et retourne un élément html
     function makeFiveDaysCast(arrayData){
         let htmlElement = ''
         arrayData.forEach(element => {
             htmlElement += `
             <div class="forecastDays bg-light text-center d-flex justify-content-evenly align-items-center rounded mb-1">
-                ${moment(element.dt_txt).locale('fr').format('ddd')}<img src="https://openweathermap.org/img/wn/${element.weather[0].icon}.png" alt="Image météo"><span><i class="bi bi-arrow-down-short"></i>${element.main.temp_min.toPrecision(1)}°C / <i class="bi bi-arrow-up-short"></i>${element.main.temp_max.toPrecision(1)}°C</span>
+                ${moment(element.dt_txt).locale('fr').format('ddd')}<img src="https://openweathermap.org/img/wn/${element.weather[0].icon}.png" alt="Image météo"><span><i class="bi bi-arrow-down-short"></i>${Math.round(element.main.temp_min)}°C / <i class="bi bi-arrow-up-short"></i>${Math.round(element.main.temp_max)}°C</span>
             </div>    
             `
         })
+        return htmlElement
+    }
+
+    // fonction permettant de créer les 10 prochaines heures de la journée
+    // elle prends en paramètre un tableau de données et le nombre d'heures à afficher et retourne un élément html
+    function makeTenHoursCast(arrayData,nbHours){
+        let htmlElement = ''
+        for(let i = 0; i < nbHours ; i++){
+            htmlElement += `
+            <div class="forecastHours bg-light border rounded py-2">
+                <p class="mb-0">${moment(arrayData.list[i].dt_txt).locale('fr').format('LT')}</p>
+                    <img src="https://openweathermap.org/img/wn/${arrayData.list[i].weather[0].icon}@2x.png" alt="Image météo">
+                <p class="mb-0">${Math.round(arrayData.list[i].main.temp)}°C</p>
+            </div>
+            `
+        }
         return htmlElement
     }
